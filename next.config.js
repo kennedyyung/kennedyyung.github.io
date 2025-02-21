b33a8433ca6ce972f0493d1b3eab8b0c7c666bfd
Kennedy Yung <kennedyyung@gmail.com>
2025-02-21 12:22:02 -0800

    fixing build errors
    Update nextjs.yml
    
    Update nextjs.yml
    
    fix
    
    Update nextjs.yml
    
    updates
    
    Update nextjs.yml
    
    Update nextjs.yml
    
    fixes
    
    Update nextjs.yml
    
    fixes
    
    Update nextjs.yml
    
    Update nextjs.yml
    
    Update nextjs.yml
    
    Update nextjs.yml
    
    Update next.config.js
    
    Update nextjs.yml
    
    Update next.config.js
    
    fixing versions
    
    Update nextjs.yml
    
    Update nextjs.yml
    
    Update nextjs.yml
    
    Update nextjs.yml
    
    Update nextjs.yml
    
    Update nextjs.yml
    
    Update nextjs.yml
    
    Update nextjs.yml
    
    Update nextjs.yml

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/kennedyyung.github.io',
-
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["images.unsplash.com"],
   },
+  basePath: '/kennedyyung.github.io', // Optional, if using a custom repo name for GitHub Pages
+  output: 'export'
 };
 
 module.exports = nextConfig;
