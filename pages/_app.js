e17197c78dd818eb44a1588317a7ecf8ded9cf0b
Kennedy Yung <kennedyyung@gmail.com>
2025-03-08 01:04:37 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,24 @@
-import "../styles/globals.css"
-import Layout from "components/layout/Layout"
-import ThemeProvider from "components/layout/ThemeProvider";
+import "../styles/globals.css";
+import Layout from "components/layout/Layout";
+import { Roboto } from 'next/font/google';
+ 
+const rubik = Roboto({
+  subsets: ['latin'],
+  variable: '--font-rubik',
+  weight: '400'
+})
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
+    <main  className={`${rubik.variable} font-sans`}>
     <Layout>
 
       <Component {...pageProps} />
 
     </Layout>
+    </main>
   )
 }
 
