08c6a7c397fd1bcad9d043ad86522ecb5d1886f9
Kennedy Yung <kennedyyung@gmail.com>
2025-03-09 03:17:32 -0700

    Update HomePage.js

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -15,7 +15,7 @@ const learningTech = [
 
 const HomePage = () => {
   return (
-    <div className=" h-screen flex justify-center items-center bg-base dark:bg-dark-base ">
+    <div className="justify-center items-center bg-base dark:bg-dark-base ">
             <Head>
         <title>Home Page</title>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-064E3270RM"></script>
