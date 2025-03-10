c120942933a4bd7a8729af12cd58bd5d63b9bb04
Kennedy Yung <kennedyyung@gmail.com>
2025-03-09 22:17:11 -0700

    updating layout of experiences

--- pages/Experience.js
+++ pages/Experience.js
@@ -9,190 +9,140 @@ const Experience = () => {
           <div className="grid place-items-center">
             <div className="font-extrabold text-5xl mb-10">Experience</div>
           </div>
-          <hr />
+          <hr/>
 
-          <div>
-            <div className="grid grid-cols-2">
-              <h1 className="font-bold text-2xl ml-10 mt-5 ">
-                Information Systems Intern
-              </h1>
-              <p className="flex justify-end mr-10 mt-5 text-lg">
-                January 2025 - present
-              </p>
-            </div>
-            <p className="font-bold italic ml-10 text-lg">
-              Engineers and Geoscientists BC
-            </p>
-
-            <ul className="list-disc list-inside ml-10 text-norm">
-              <li> Technology Stack: C#, KendoUI, JavaScript, React.Js, SQL</li>
-            </ul>
-          </div>
-
-          <div>
-            <h1 className="font-bold text-2xl ml-10 mt-5 ">
-              UBC BEST Design Team
-            </h1>
-            <ul className="list-disc list-inside ml-10 ">
-              <li>
-                {" "}
-                Biomedical engineering team developing a data-driven
-                hand-assisting device, along with a companion app and games, to
-                enhance hand rehabilitation for neurodivergent patients and
-                provide progress tracking for both patients and physicians
-              </li>
-              <li> Secured second place at Simon Cox Competition 2024</li>
-            </ul>
-
-            <div className="grid grid-cols-2 mt-2">
-              <p className="font-bold text-lg italic ml-10">M2M: Co-Lead</p>
-              <p className="flex justify-end mr-10 text-lg ">
-                September 2024 - present
-              </p>
-            </div>
-
-            <ul className="list-disc list-inside ml-10">
-              <li>
-                {" "}
-                Overseeing the team&apos;s software and hardware development,
-                and external relations
+          <ul className="timeline timeline-vertical mt-20 ">
+  <li >
+  <div className=" collapse-arrow timeline-start timeline-box w-2/3 collapse dark:text-black">
+  <input type="checkbox" />
+  <div className="collapse-title text-lg">Information Systems Intern</div>
+  <div className="collapse-content text-sm">
+    <p className="font-bold">Engineers and Geoscientists BC</p>
+    <ul className="list-disc list-inside">
+      <li> Developing a custom Outlook add-on, reducing annual liscensing costs </li>
+      <li>Implementing new features and bug fixes on internal and external applications using KendoUI, MVC.Net, and 
+      React.js </li>
+    </ul>
+  </div>
+</div>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+      </svg>
+    </div>
+    <hr className=" my-6 bg-sidebar"/>
+    <div className="timeline-end text-sm">Jan 2025 - Present</div>
+    <hr className=" my-6 bg-sidebar"/>
+  </li>
+  <li>
+    <hr className=" bg-sidebar"/>
+    <div className="timeline-start text-sm">Sept 2024 - Present</div>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+      </svg>
+    </div>
+    <div className=" collapse-arrow timeline-end timeline-box w-2/3 collapse dark:text-black">
+  <input type="checkbox" />
+  <div className="collapse-title text-lg">M2M: Co-lead</div>
+  <div className="collapse-content text-sm">
+    <p className="font-bold">UBC BEST Design Team</p>
+    <ul className="list-disc list-inside ">
+              <li>Leading the design and development of our games, ensuring seamless user experience
               </li>
               <li>
-                {" "}
-                Lead the design and development of our games, ensuring
-                high-quality gameplay and seamless user experience
+              Leading the redesign and migration of our React Native app to Flutter, improving performance and cross-platform consistency
               </li>
-              <li>
-                {" "}
-                Leading the redesign and migration of our React Native app to
-                Flutter while maintaining existing functionality, improving
-                performance and ensuring cross-platform consistency
-              </li>
-              <li>
-                {" "}
-                Implemented agile methodologies using Jira as co-lead,
-                optimizing task tracking and team efficiency
-              </li>
-              <li>
-                {" "}
-                Engage with entrepreneurship@UBC through workshops and
-                mentorship to explore commercialization opportunities for our
-                product
+              <li>Engaging with entrepreneurship@UBC through workshops to explore commercialization opportunities
               </li>
             </ul>
-          </div>
-          <div>
-            <div className="grid grid-cols-2">
-              <p className="text-lg font-bold italic ml-10 mt-2">
-                M2M: Software Engineer
-              </p>
-              <p className="flex justify-end mr-10 text-lg">
-                October 2023 - Sept 2024
-              </p>
-            </div>
+  </div>
+</div>
+    <hr className=" bg-sidebar"/>
+  </li>
 
-            <ul className="list-disc list-inside ml-10">
-              <li>
-                {" "}
-                Established a modular UI component library with React Native for
-                the application, improving development speed
+  <li>
+    <hr className=" bg-sidebar" />
+    <div className="timeline-end text-sm">May 2024 - Dec 2024</div>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+      </svg>
+    </div>
+    <div className=" collapse-arrow timeline-start timeline-box w-2/3 collapse dark:text-black">
+  <input type="checkbox" />
+  <div className="collapse-title text-lg">Front-end Web Developer Intern</div>
+  <div className="collapse-content text-sm">
+    <p className="font-bold">UBC Faculty of Applied Science</p>
+    <ul className="list-disc list-inside ">
+    <li>Developed customizable Twig templates for websites, reducing website creation time
+    </li>
+              <li>Streamlined workflow with Atlassian Confluence, Jira, and Git, following Software Development Life Cycle(SDLC) practices
+              </li>
+              <li>Reviewed peer code submissions, implementing a feedback loop; resulted in improved code and expedited timelines
+              </li>
+              <li>Increased engagement across multiple faculty websites by 15% through targeted improvements
               </li>
               <li>
-                {" "}
-                Enhanced functionality and resolved technical problems within
-                the games coded in C# through Unity{" "}
-              </li>
-              <li>
-                {" "}
-                Designed and created vectors and a background image for one of
-                the games{" "}
-              </li>
-              <li>
-                {" "}
-                Technology Stack: React Native, C# and used Unity3D application
+              Collaborated with developers, designers, and PMs using Agile methodologies to efficiently manage timelines
               </li>
             </ul>
-          </div>
-          <div>
-            <div className="grid grid-cols-2">
-              <h1 className="font-bold text-2xl ml-10 mt-5 ">
-                Front-End Web Developer
-              </h1>
-              <p className="flex justify-end mr-10 mt-5 text-lg">
-                May 2024 - December 2024
-              </p>
-            </div>
-            <p className="text-lg font-bold italic ml-10">
-              UBC Faculty of Applied Science
-            </p>
-
-            <ul className="list-disc list-inside ml-10">
-              <li>
-                {" "}
-                Developed and maintained 5 websites using PHP, CSS, Node.js,
-                MariaDB, and Drupal{" "}
+  </div>
+</div>
+    <hr className=" bg-sidebar"/>
+  </li>
+  <li>
+    <hr className=" bg-sidebar"/>
+    <div className=" collapse-arrow timeline-end timeline-box w-2/3 collapse dark:text-black">
+  <input type="checkbox" />
+  <div className="collapse-title text-lg">M2M: Software Engineer</div>
+  <div className="collapse-content text-sm">
+    <p className="font-bold">UBC BEST Design Team</p>
+    <ul className="list-disc list-inside ">
+              <li>Established a modular UI component library with React Native for the application, improving development speed
               </li>
               <li>
-                {" "}
-                Collaborated with developers, designers, and project managers
-                using Agile methodologies to efficiently manage timelines{" "}
+              Enhanced functionality and resolved technical problems within the games coded in C# through Unity
               </li>
-              <li>
-                {" "}
-                Streamlined workflow with Atlassian Confluence, Jira, and Git,
-                following Software Development Life Cycle(SDLC) practices{" "}
-              </li>
-              <li>
-                {" "}
-                Reviewed peer code submissions, implementing a feedback loop;
-                resulted in improved code and expedited timelines
-              </li>
-              <li>
-                {" "}
-                Led client meetings to ensure alignment with their needs
-                throughout the website development process{" "}
-              </li>
-              <li>
-                {" "}
-                Increased engagement across multiple faculty websites by 15%
-                through targeted improvements
-              </li>
-              <li>
-                {" "}
-                Developed customizable Twig templates for website content in
-                Drupal, reducing website creation time
-              </li>
-              <li>
-                {" "}
-                Technology Stack: PHP and Twig templates, CSS and TailwindCSS,
-                Node.js, MariaDB, Drupal, Atlassian products
+              <li>Designed and created vectors and background images
               </li>
             </ul>
-          </div>
+  </div>
+</div>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+      </svg>
+    </div>
+    <hr className=" my-6 bg-sidebar"/>
+    <div className="timeline-start text-sm">Oct 2023 - Sept 2024</div>
+  </li>
+  <li>
+    <hr className=" bg-sidebar"/>
+    <div className=" collapse-arrow timeline-start timeline-box w-2/3 collapse dark:text-black">
+  <input type="checkbox" />
+  <div className="collapse-title text-lg">Student Developer</div>
+  <div className="collapse-content text-sm">
+    <p className="font-bold">UBC Computer Science Society</p>
+    <ul className="list-disc list-inside ">
+              <li>Collaborating within a team to develop a website for UBC Tennis Circle Club
+              </li>
+              <li>
+              Programmed using Next.js and Typescript, with MongoDB for the database, and ChakraUI for UI framework
+              </li>
+      </ul>
+  </div>
+</div>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+      </svg>
+    </div>
+    <hr className=" my-6 bg-sidebar"/>
+    <div className="timeline-end text-sm">Nov 2023 - Apr 2024</div>
+  </li>
+  </ul>
 
-          <div>
-            <div className="grid grid-cols-2">
-              <h1 className="font-bold ml-10 mt-5 text-2xl">
-                Student Developer
-              </h1>
-              <p className="flex justify-end mr-10 mt-5 text-lg">
-                November 2023 - April 2024
-              </p>
-            </div>
-            <p className="font-bold italic ml-10 text-lg">
-              UBC Computer Science Society
-            </p>
-            <ul className="list-disc list-inside ml-10">
-              <li>
-                Collaborating within a team to develop a website for UBC Tennis
-                Circle Club
-              </li>
-              <li>
-                Programmed using Next.js and Typescript, with MongoDB for the
-                database, and ChakraUI for UI framework
-              </li>
-            </ul>
-          </div>
         </div>
       </div>
     </div>
